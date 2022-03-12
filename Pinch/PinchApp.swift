498416a312c5c71ba7c0c23c027dab3dcbb1cd08
Anuj Soni <anujsoni10001@gmail.com>
2022-03-12 14:49:20 +0530

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Anuj Soni on 12/03/22.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
